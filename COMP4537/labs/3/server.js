e3341757e643bc75251016386a4c30b38b2eaeda
Emily0816Lin <emilysmileee@gmail.com>
2024-09-29 16:15:46 -0700

    finished partB and C, now test hosting

--- COMP4537/labs/3/server.js
+++ COMP4537/labs/3/server.js
@@ -102,9 +102,8 @@ const server = http.createServer((req, res) => {
 });
 
 
-
-server.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+// server.listen(3000, () => {
+//   console.log('Server is running on port 3000');
+// });
 
 module.exports = server;
