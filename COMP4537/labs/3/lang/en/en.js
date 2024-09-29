7d91f6cdf4eee62d4ff21b1e5f332a02aa8c8ffb
Emily0816Lin <emilysmileee@gmail.com>
2024-09-29 13:30:16 -0700

    test the messages

--- COMP4537/labs/3/lang/en/en.js
+++ COMP4537/labs/3/lang/en/en.js
@@ -1,14 +1,14 @@
 // Disclosure: I used ChatGPT to assist with the content of this assignment.
 
-const messages = {
-    greeting: 'Hello %1, What a beautiful day. Server current date and time is',
-};
-
-export default messages;
-
-
-// lang/en/en.js
-// module.exports = {
+// const messages = {
 //     greeting: 'Hello %1, What a beautiful day. Server current date and time is',
-//   };
+// };
+
+// export default messages;
+
+
+lang/en/en.js
+module.exports = {
+    greeting: 'Hello %1, What a beautiful day. Server current date and time is',
+  };
   
--- COMP4537/labs/3/server.js
+++ COMP4537/labs/3/server.js
@@ -1,12 +1,12 @@
 // Disclosure: I used ChatGPT to assist with the content of this assignment.
 
-import messages from '../lang/en/en.js';
+// import messages from '../lang/en/en.js';
 
 // server.js
 const http = require('http');
 const url = require('url');
 const { getDate } = require('./modules/utils');
-// const messages = require('./lang/en/en');
+const messages = require('./lang/en/en');
 
 const server = http.createServer((req, res) => {
   const queryObject = url.parse(req.url, true).query;
@@ -14,6 +14,7 @@ const server = http.createServer((req, res) => {
 
   // Set response header for HTML content
   res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Welcome to the Node.js HTTP Server!</h1>');
 
   // Create a blue-colored response
   const message = `<p style="color:blue">${messages.greeting.replace('%1', name)} ${getDate()}</p>`;
